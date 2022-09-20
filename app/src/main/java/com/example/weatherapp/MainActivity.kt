a3379f2b892e79ebef9ab4cb2e68a5d692cd94c1
Vitor Machado <usl.contatos@gmail.com>
2022-09-19 21:46:14 -0300

    Finaliza de verdade

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -13,6 +13,8 @@ import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import org.json.JSONObject
 import java.net.URL
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -39,13 +41,9 @@ class MainActivity : AppCompatActivity() {
             setMessage(message)
             show()
         }
-
-
     }
-
+    //Classe para o imput de dados da API
     inner class weatherTask() : AsyncTask<String, Void, String>() {
-
-
 
         override fun onPreExecute() {
             findViewById<ProgressBar>(R.id.loader).visibility = View.VISIBLE
@@ -93,75 +91,35 @@ class MainActivity : AppCompatActivity() {
 
                 }  }
 
-
             return response
-
 
         }
 
-
-
-
-        //Faz a leitura do dados e modifica os textos da HomeScreen
+        //Faz a leitura do dados e modifica as infos da tela do APP
         @RequiresApi(Build.VERSION_CODES.O)
         override fun onPostExecute(result: String?) {
 
 
 
             try {
-                //Extraindo dados do JSON da API
                 val jsonObj = JSONObject(result)
                 val today = jsonObj.getJSONArray("list").getJSONObject(index)
                 val main = today.getJSONObject("main")
-                val wind = today.getJSONObject("wind")
                 val weather = today.getJSONArray("weather").getJSONObject(0)
                 val city = jsonObj.getJSONObject("city")
                 val updateAt = today.getString("dt_txt")
 
-
-
-                //DetailsContainer
-                val windSpeed = wind.getString("speed")
                 val humidity = main.getString("humidity")
                 val pressure = main.getString("pressure")
 
-                //OverviewContainer
                 val status = weather.getString("description")
-                //val status = "Thunderstorm"
                 val temp = main.getString("temp").substring(0,2)
                 val temp_min = main.getString("temp_min").substring(0,2)
                 val temp_max = main.getString("temp_max").substring(0,2)
                 val address = city.getString("name")+", "+ city.getString("country")
 
-                //ForecastContainer
-                val day1 = jsonObj.getJSONArray("list").getJSONObject(1)
-                val main1 = day1.getJSONObject("main")
-                val weather1 = day1.getJSONArray("weather").getJSONObject(0)
-                val status1 = weather1.getString("main")
-                val temp1 = main1.getString("temp").substring(0,2)
 
-                val day2 = jsonObj.getJSONArray("list").getJSONObject(2)
-                val main2 = day2.getJSONObject("main")
-                val weather2 = day2.getJSONArray("weather").getJSONObject(0)
-                val status2 = weather2.getString("main")
-                val temp2 = main2.getString("temp").substring(0,2)
-
-                val day3 = jsonObj.getJSONArray("list").getJSONObject(3)
-                val main3 = day3.getJSONObject("main")
-                val weather3 = day3.getJSONArray("weather").getJSONObject(0)
-                val status3 = weather3.getString("main")
-                val temp3 = main3.getString("temp").substring(0,2)
-
-                val day4 = jsonObj.getJSONArray("list").getJSONObject(4)
-                val main4 = day4.getJSONObject("main")
-                val weather4 = day4.getJSONArray("weather").getJSONObject(0)
-                val status4 = weather4.getString("main")
-                val temp4 = main4.getString("temp").substring(0,2)
-
-
-
-                //Exibindo os dados em tela
-                //findViewById<TextView>(R.id.wind).text = windSpeed + " km/h"
+                //Exibindo os dados na tela
                 findViewById<TextView>(R.id.humidity).text = humidity + "%"
                 findViewById<TextView>(R.id.pressure).text = pressure + " mb"
 
@@ -174,10 +132,18 @@ class MainActivity : AppCompatActivity() {
                 findViewById<TextView>(R.id.temp_max).text = "Max: " + temp_max + "°"
                 findViewById<TextView>(R.id.address).text = address
                 findViewById<TextView>(R.id.status).text = status.capitalize()
-                findViewById<TextView>(R.id.updated_at).text = updateAt
 
 
 
+
+
+                val isoFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss")
+                val date = isoFormat.parse(updateAt.replace(" ", "T"))
+                isoFormat.timeZone = TimeZone.getTimeZone("GMT-3")
+                isoFormat.applyPattern(" 'Dia' dd, 'às' HH'h'.");
+                val formattedDate = isoFormat.format(date)
+
+                findViewById<TextView>(R.id.updated_at).text = formattedDate
 
 
                 // -------------------------------------------------- //
